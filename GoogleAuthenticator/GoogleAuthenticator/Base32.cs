58b011e76a632ed5b7128af62d0fdbcf57eaa603
LTruijens_cp <SND\LTruijens_cp>
2011-11-28 19:22:52 +0000

    Initial check in

---
+++ GoogleAuthenticator/GoogleAuthenticator/Base32.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoogleAuthenticator
+{
+    // https://code.google.com/p/cc-sharp/source/browse/trunk/trunk/src/Transcoder.cs?r=2
+    public static class Base32
+    {
+        private const int IN_BYTE_SIZE = 8;
+        private const int OUT_BYTE_SIZE = 5;
+        private static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
+
+        public static string Encode(byte[] data)
+        {
+            int i = 0, index = 0, digit = 0;
+            int current_byte, next_byte;
+            StringBuilder result = new StringBuilder((data.Length + 7) * IN_BYTE_SIZE / OUT_BYTE_SIZE);
+
+            while (i < data.Length)
+            {
+                current_byte = (data[i] >= 0) ? data[i] : (data[i] + 256); // Unsign
+
+                /* Is the current digit going to span a byte boundary? */
+                if (index > (IN_BYTE_SIZE - OUT_BYTE_SIZE))
+                {
+                    if ((i + 1) < data.Length)
+                        next_byte = (data[i + 1] >= 0) ? data[i + 1] : (data[i + 1] + 256);
+                    else
+                        next_byte = 0;
+
+                    digit = current_byte & (0xFF >> index);
+                    index = (index + OUT_BYTE_SIZE) % IN_BYTE_SIZE;
+                    digit <<= index;
+                    digit |= next_byte >> (IN_BYTE_SIZE - index);
+                    i++;
+                }
+                else
+                {
+                    digit = (current_byte >> (IN_BYTE_SIZE - (index + OUT_BYTE_SIZE))) & 0x1F;
+                    index = (index + OUT_BYTE_SIZE) % IN_BYTE_SIZE;
+                    if (index == 0)
+                        i++;
+                }
+                result.Append(alphabet[digit]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ GoogleAuthenticator/GoogleAuthenticator/BytesToStringConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Windows.Markup;
+using System.Runtime.Remoting;
+
+namespace GoogleAuthenticator
+{
+    [ValueConversion(typeof(byte[]), typeof(string))]
+    public class BytesToStringConverter: MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+               return BytesToString((byte[])value);
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (((string)value).Length % 2 == 0)
+                return StringToBytes((string)value);
+            else
+                return Binding.DoNothing;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        // http://stackoverflow.com/a/2556329/1242
+        public byte[] StringToBytes(string value)
+        {
+            try
+            {
+                SoapHexBinary shb = SoapHexBinary.Parse(value);
+                return shb.Value;
+            }
+            catch (RemotingException)
+            {
+                return new byte[0];
+            }
+        }
+
+        public string BytesToString(byte[] value)
+        {
+            SoapHexBinary shb = new SoapHexBinary(value);
+            return shb.ToString();
+        }
+    }
+}
